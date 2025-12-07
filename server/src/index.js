1113481bc2b3e92f835cd009ae9fa94f01f19757
Cavan P <53267753+Cavan-P@users.noreply.github.com>
2025-12-07 17:25:43 -0600

    preparing for vps

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const attachWebSocket = require('./websocket')
 const app = express()
 app.use(express.static(path.join(__dirname, '../../client')))
 
-const server = app.listen(8000, _ => {
+const server = app.listen(8000, '0.0.0.0', _ => {
     console.log('Server running on port 8000')
 })
 
